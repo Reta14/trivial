2558679d3d74ab5285d8b9f69d70802c541ea91d
marc_14 <retilla_20@hotmail.com>
2024-05-08 17:44:11 +0200

    habilitar boto tornar a jugar

--- Client/src/main/java/marc/nikita/trivial/controladors/Controlador_Vista_4.java
+++ Client/src/main/java/marc/nikita/trivial/controladors/Controlador_Vista_4.java
@@ -1,8 +1,14 @@
 package marc.nikita.trivial.controladors;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import marc.nikita.trivial.Classes.Client;
+import marc.nikita.trivial.HelloApplication;
+
+import java.io.IOException;
 
 public class Controlador_Vista_4 {
     @FXML
@@ -20,6 +26,18 @@ public class Controlador_Vista_4 {
         System.exit(0);
     }
 
+    @FXML
+    private void handleTornaAJugarButtonAction() {
+        try {
+            // Cargar la vista inicial y mostrarla
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("Vista_1.fxml"));
+            Stage stage = (Stage) idTornaAJugar.getScene().getWindow();
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
